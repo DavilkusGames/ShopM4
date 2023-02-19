f2ca384c0feed566d3e88f1d6e2050f09aa7a970
Davilkus Games <davilkusgames@gmail.com>
2023-02-19 19:53:48 +0300

    uppdate

---
+++ ShopM4_DataMigrations/Repository/IRepository/IRepositoryMyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopM4_Models;
+
+namespace ShopM4_DataMigrations.Repository.IRepository
+{
+    internal interface IRepositoryMyModel : IRepository<MyModel>
+    {
+        void Update(Category obj); 
+    }
+}
---
+++ ShopM4_DataMigrations/Repository/RepositoryMyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ShopM4_DataMigrations.Data;
+using ShopM4_DataMigrations.Repository.IRepository;
+using ShopM4_Models;
+
+namespace ShopM4_DataMigrations.Repository
+{
+    internal class RepositoryMyModel : Repository<Category>, IRepositoryCategory
+    {
+    }
+}
